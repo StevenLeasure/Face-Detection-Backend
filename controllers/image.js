2aa15bd623d433b50305cf19353a5188a1312513
Steven Coding <StevenCoding@SauceMac.local>
2025-09-07 23:09:32 -0400

    Initial Upload

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+// const Clarifai = require('clarifai');
+
+// const returnClarifaiRequestOptions = (imageUrl) => {
+//   // Your PAT (Personal Access Token) can be found in the Account's Security section
+//   const PAT = 'f994193246d54eff9d623415ba286671';
+//   // Specify the correct user_id/app_id pairings
+//   // Since you're making inferences outside your app's scope
+//   const USER_ID = 'steven_leasure';
+//   const APP_ID = 'main';
+//   // Change these to whatever model and image URL you want to use
+//   const MODEL_ID = 'face-detection';
+//   const IMAGE_URL = imageUrl;
+
+const handleImage = (db) => (req, res) => {
+	const {id} = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage
+}
---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db =knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'StevenCoding',
+    port: 5432,
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+db.select('*').from('users').then(data => {
+	// console.log(data);
+});
+
+const app = express();
+app.use(cors());
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {res.send(database.users)})
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.post('/register', register.handleRegister(db, bcrypt))
+
+app.get('/profile/:id', profile.handleProfileGet(db))
+
+app.put('/image', image.handleImage(db))
+
+app.post('/api/clarifai', async (req, res) => {
+  const { imageUrl, requestOptions } = req.body;
+  const PAT = 'f994193246d54eff9d623415ba286671'; // PAT stays only on server
+
+  try {
+    // Add the Authorization header with PAT on the server side
+    const response = await fetch("https://api.clarifai.com/v2/models/face-detection/outputs", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT, // PAT added here on server
+        'Content-Type': 'application/json'
+      },
+      body: requestOptions.body // Use the body from frontend request
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Clarifai API error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Clarifai API error:', error);
+    res.status(500).json({ error: 'Failed to process image: ' + error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
+});
+
+/*
+--Plan-- 
+/ --> res = this is working
+/signin --> POST = success or fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
